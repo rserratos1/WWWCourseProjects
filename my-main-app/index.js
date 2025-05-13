5026473a163efd07d50f42c56a1d5dae840e9bd6
Raquel Serratos <168024719+rserratos1@users.noreply.github.com>
2025-05-12 17:07:38 -0700

    Update index.js

--- my-main-app/index.js
+++ my-main-app/index.js
@@ -1,1 +1,13 @@
-<p>Hello !!!!!!!!!!!!!!!!! 😊😊😊😊😊😊😊😊😊</p>;
+// my-main-app/index.js
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// serve your HTML on /
+app.get('/', (req, res) => {
+  res.send('<p>Hello !!!!!!!!!!!!! 🙂🙂🙂🙂🙂🙂🙂🙂🙂</p>');
+});
+
+app.listen(port, () =>
+  console.log(`Listening on port ${port}`)
+);
