f4a7e4a1387fa7a9010cf007f4483579a59fe1df
rserratos1 <rserratos1@toromail.csudh.edu>
2025-05-13 00:18:12 -0700

    fixed the screen error

--- my-main-app/src/app/landingPage/index.js
+++ my-main-app/src/app/landingPage/index.js
@@ -1,7 +1,4 @@
-// const BACKGROUNDS = ["ice1.jpeg", "ice2.jpeg", "ice3.jpeg"];
-
 export default function landingPage() {
-  // const choice = BACKGROUNDS[Math.floor(Math.random() * BACKGROUNDS.length)];
   return `<!DOCTYPE html>
 <html lang="en">
   <head>
@@ -11,11 +8,7 @@ export default function landingPage() {
     <title>MyProjects</title>
   </head>
   
-  <body
-  // style="
-  //     margin:0;
-  //     background: url('/images/${choice}') center/cover no-repeat;
-  //   ">
+  <body>
     <img src="/images/penguin.png" class="penguin" alt="Penguin" />
 
     <main>
