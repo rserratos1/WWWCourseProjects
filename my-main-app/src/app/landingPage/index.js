ba458088f9ed551c77fa56d23d870c2662d35e2b
rserratos1 <rserratos1@toromail.csudh.edu>
2025-05-12 19:38:27 -0700

    new style sheet for landing page

--- my-main-app/src/app/landingPage/index.js
+++ my-main-app/src/app/landingPage/index.js
@@ -4,17 +4,13 @@ export default function landingPage() {
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Playwrite+DK+Loopet:wght@100..400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/css/landing.css" />
     <title>Landing</title>
   </head>
   
   <body>
     <main >
-      <div 
-        style="background-color: #4f46e5; color: #fff; text-align: center; font-family:'Playwrite DK Loopet', cursive; font-size: 20px; padding: 20px; border-radius: 10px;"
-      >
+      <div class="container">
         Hello, this is Assignment 5! 😊
       </div>
     </main>
