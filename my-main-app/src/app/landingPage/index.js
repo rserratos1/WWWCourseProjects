0258280e66ae15c26fb4fa7a88c053bc3d750b2d
rserratos1 <rserratos1@toromail.csudh.edu>
2025-05-12 19:16:41 -0700

    updating font

--- my-main-app/src/app/landingPage/index.js
+++ my-main-app/src/app/landingPage/index.js
@@ -5,13 +5,16 @@ export default function landingPage() {
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="/styles.css" rel="stylesheet" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playwrite+DK+Loopet:wght@100..400&display=swap" rel="stylesheet">
     <title>Landing</title>
   </head>
   
   <body>
     <main class="flex justify-center items-center h-screen">
       <div 
-        style="background-color: #4f46e5; color: #fff; text-align: center; font-size: 20px; padding: 20px; border-radius: 10px;" 
+        style="background-color: #4f46e5; color: #fff; text-align: center; font-family:'Playwrite DK Loopet', cursive; font-size: 20px; padding: 20px; border-radius: 10px;" 
         class="flex flex-col items-center justify-center w-full md:w-140 xl:w-200 bg-indigo-300 inset-shadow inset-shadow-indigo-950"
       >
         Hello, this is Assignment 5! 😊
