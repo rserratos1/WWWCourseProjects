b5028148fbc2cf020b03be74b5d416aa76b286f1
rserratos1 <rserratos1@toromail.csudh.edu>
2025-05-12 17:45:39 -0700

    testing landingPage

--- my-main-app/index.js
+++ my-main-app/index.js
@@ -1,13 +1,14 @@
+import { landingPage } from "./landing-page.js";
+
 // my-main-app/index.js
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
 // serve your HTML on /
-app.get('/', (req, res) => {
-  res.send('<p>Hello !!!!!!!!!!!!! 🙂🙂🙂🙂🙂🙂🙂🙂🙂</p>');
+app.get("/", (req, res) => {
+  res.send("<p>Hello !!!!!!!!!!!!! 🙂🙂🙂🙂🙂🙂🙂🙂🙂</p>");
+  <landingPage />;
 });
 
-app.listen(port, () =>
-  console.log(`Listening on port ${port}`)
-);
+app.listen(port, () => console.log(`Listening on port ${port}`));
--- my-main-app/src/app/landingPage/index.js
+++ my-main-app/src/app/landingPage/index.js
@@ -1,7 +1,9 @@
 export default function landingPage() {
   return (
-    <main className="flex justify-center bg-gray-100">
-      <div className="flex flex-col w-full md:w-140 xl:w-200 bg-white justify-center inset-shadow inset-shadow-indigo-950"></div>
+    <main className="flex justify-center bg-indigo-300 h-screen">
+      <div className="flex flex-col w-full md:w-140 xl:w-200 bg-white justify-center inset-shadow inset-shadow-indigo-950">
+        TEST
+      </div>
     </main>
   );
 }
