269252c578ba8dc86e8f5151bb2cfe77b1e7f74e
rserratos1 <rserratos1@toromail.csudh.edu>
2025-05-12 18:35:05 -0700

    updating tailwindcss

---
+++ my-main-app/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/landingPage/**/*.js",
+    "./src/**/*.{js,html}",
+    "./public/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
